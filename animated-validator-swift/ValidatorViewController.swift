8e10d6cc8f1db47af2807bc7d379d1c2814deab0
Taniraw <tanirawrites@gmail.com>
2016-10-17 19:46:26 -0400

    Animated Validator Completed

--- animated-validator-swift/ValidatorViewController.swift
+++ animated-validator-swift/ValidatorViewController.swift
@@ -17,10 +17,13 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.x
+        
         self.submitButton.accessibilityLabel = Constants.SUBMITBUTTON
         self.emailTextField.accessibilityLabel = Constants.EMAILTEXTFIELD
         self.emailConfirmationTextField.accessibilityLabel = Constants.EMAILCONFIRMTEXTFIELD
@@ -28,7 +31,84 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
         self.passwordTextField.accessibilityLabel = Constants.PASSWORDTEXTFIELD
         self.passwordConfirmTextField.accessibilityLabel = Constants.PASSWORDCONFIRMTEXTFIELD
         
-        self.submitButton.isEnabled = false
+        self.submitButton.isEnabled = true
+    }
+  
+    
+    @IBAction func emailTextInput(_ sender: AnyObject) {
+        if (emailTextField.text?.contains("@"))! && (emailTextField.text?.contains("."))!{
+            emailTextField.backgroundColor = UIColor.green
+            } else {
+                emailTextField.shake(count: 4, for: 1.0, withTanslation: 10)
+                emailTextField.backgroundColor = UIColor.red
+            }
+        }
+    
+
+    @IBAction func confirmEmail(_ sender: AnyObject) {
+        if emailConfirmationTextField.text == emailTextField.text {
+            emailConfirmationTextField.backgroundColor = UIColor.green
+        } else{
+            emailConfirmationTextField.shake(count: 4, for: 1.0, withTanslation: 10)
+            emailConfirmationTextField.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func inputPhone(_ sender: AnyObject) {
+        if ((phoneTextField.text?.characters.count)! == 11) && (Int((phoneTextField.text)!)  != nil) {
+            phoneTextField.backgroundColor = UIColor.green
+            print("valid")
+        } else {
+            phoneTextField.shake(count: 4, for: 1.0, withTanslation: 10)
+            phoneTextField.backgroundColor = UIColor.red
+            print("invalid")
+        }
+            }
+    
+    @IBAction func inputPassword(_ sender: AnyObject) {
+        if (passwordTextField.text?.characters.count)! >= 6{
+            passwordTextField.backgroundColor = UIColor.green
+        } else {
+            passwordTextField.shake(count: 4, for: 1.0, withTanslation: 10)
+            passwordTextField.backgroundColor = UIColor.red
+        }
+        
+    }
+    
+    @IBAction func confirmPassword(_ sender: AnyObject) {
+        if passwordConfirmTextField.text == passwordTextField.text{
+            passwordConfirmTextField.backgroundColor = UIColor.green
+        } else{
+            passwordConfirmTextField.shake(count: 4, for: 1.0, withTanslation: 10)
+            passwordConfirmTextField.backgroundColor = UIColor.red
+        }
+    }
+    
+}
+
+public extension UIView {
+    
+    func shake(count : Float? = nil,for duration : TimeInterval? = nil,withTanslation translation : Float? = nil) {
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        animation.repeatCount = count ?? 2
+        animation.duration = (duration ?? 0.5)/TimeInterval(animation.repeatCount)
+        animation.autoreverses = true
+        animation.byValue = translation ?? -5
+        layer.add(animation, forKey: "shake")
+        
     }
 
-}
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
